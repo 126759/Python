197be0b511a393d7fe27bd2376d98233591c49b2
SREYC | Sahil Rey Chandnani <31300187+Sahilreyc@users.noreply.github.com>
2018-11-09 11:20:58 +0800

    Add files via upload

--- Phyton Challenges/Vending Machine.py
+++ Phyton Challenges/Vending Machine.py
@@ -1,23 +1,26 @@
-#Vending Machine
+item = float(input("What item do you want? Crisps(1), Coke(2), Sprite(3), Sweets(4), Jerkey(5)"))
 
-VM = input("What item would you like? Lays(1), Coke(2), Ice Lemon Tea(3), Kit Kat(4), Sprite(5)")
-if VM == 1:
-    print("Enjoy your Lays")
+if item == 1:
 
-elif VM == 2:
-    print("Enjoy your Coke")
+    price1 = ("$2.30")
+    print("Please pay", price1)
 
-elif VM == 3:
-    print("Enjoy your Ice Lemon Tea")
+elif item == 2:
 
-elif VM == 4:
-    print("Enjoy your Kit Kat")
+    price2 = ("$3.30")
+    print("Please pay", price2)
 
-elif VM == 5:
-    print("Enjoy your Sprite")
+elif item == 3:
+
+    price3 = ("$1.30")
+    print("Please pay", price3)
+
+elif item == 4:
+
+    price4 = ("$4.30")
+    print("Please pay", price4)
     
+elif item == 5:
 
-    
-                   
-
-                      
+    price5 = ("$5.30")
+    print("Please pay", price5)
