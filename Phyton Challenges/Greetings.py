ab224dd8fca1e1445ba9576ff988176a41347e32
SREYC | Sahil Rey Chandnani <31300187+Sahilreyc@users.noreply.github.com>
2018-11-22 20:30:51 +0800

    Update Greetings.py

--- Phyton Challenges/Greetings.py
+++ Phyton Challenges/Greetings.py
@@ -1,9 +1,9 @@
 print("Hello, I am Phyton")
 
-name==int(input("What is your name?"))
+name = int(input("What is your name?"))
 
 print("Hello",name)
 
-age==int(input("How old are you?"))
+age = int(input("How old are you?"))
 
 print("Thats good to know!")
